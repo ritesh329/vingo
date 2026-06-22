cb65ae6d0f1e45833ac4aa5fe3f191b44dfd41a1
ritesh329 <145488174+ritesh329@users.noreply.github.com>
2026-06-22 17:51:17 +0530

    Update multer.js

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,15 +1,43 @@
-import multer from "multer";
-import path from "path";
+// import multer from "multer";
+// import path from "path";
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    // ✅ Files ./public/uploads में save होंगे
-    cb(null, path.join(process.cwd(), "public", "uploads"));
-  },
-  filename: (req, file, cb) => {
-    // ✅ Unique filename बन जाएगा (timestamp + originalname)
-    cb(null, Date.now() + "-" + file.originalname);
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     // ✅ Files ./public/uploads में save होंगे
+//     cb(null, path.join(process.cwd(), "public", "uploads"));
+//   },
+//   filename: (req, file, cb) => {
+//     // ✅ Unique filename बन जाएगा (timestamp + originalname)
+//     cb(null, Date.now() + "-" + file.originalname);
+//   },
+// });
+
+// export const upload = multer({ storage });
+
+
+// import multer from "multer";
+// import path from "path";
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     // ✅ Files ./public/uploads में save होंगे
+//     cb(null, path.join(process.cwd(), "public", "uploads"));
+//   },
+//   filename: (req, file, cb) => {
+//     // ✅ Unique filename बन जाएगा (timestamp + originalname)
+//     cb(null, Date.now() + "-" + file.originalname);
+//   },
+// });
+
+// export const upload = multer({ storage });
+
+import multer from "multer";
+
+const storage = multer.memoryStorage();
+
+export const upload = multer({
+  storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5 MB
   },
 });
-
-export const upload = multer({ storage });
